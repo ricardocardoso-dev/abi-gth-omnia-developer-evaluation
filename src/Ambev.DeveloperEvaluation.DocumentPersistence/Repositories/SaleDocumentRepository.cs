28e22dca5a6667dadf09a18f3b02cd6b3aa6c0cf
Ricardo Cardoso <ricardo.cardoso@live.com>
2025-04-27 18:43:21 -0300

    feat(infrastructure): create layer for NoSQL document persistence

---
+++ src/Ambev.DeveloperEvaluation.DocumentPersistence/Repositories/SaleDocumentRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace Ambev.DeveloperEvaluation.DocumentPersistence.Repositories;
+/// <summary>
+/// Repository specialized for handling SaleDocumentDto persistence.
+/// </summary>
+public class SaleDocumentRepository//: ISaleDocumentRepository
+{
+    //private readonly IMongoCollection<SaleDocument> _collection;
+    //private readonly IMapper _mapper;
+
+    //public SaleDocumentRepository(IMongoDatabase database, IMapper mapper)
+    //{
+    //    _collection = database.GetCollection<SaleDocument>("sales");
+    //    _mapper = mapper;
+    //}
+
+    //public async Task<SaleDocumentDto> CreateAsync(SaleDocumentDto document, CancellationToken cancellationToken = default)
+    //{
+    //    var entity = _mapper.Map<SaleDocument>(document);
+    //    await _collection.InsertOneAsync(entity, cancellationToken: cancellationToken);
+    //    return document;
+    //}
+
+    //public async Task<SaleDocumentDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    //{
+    //    var filter = Builders<SaleDocument>.Filter.Eq(x => x.Id, id);
+    //    var entity = await _collection.Find(filter).FirstOrDefaultAsync(cancellationToken);
+
+    //    if (entity == null)
+    //        return default;
+
+    //    return _mapper.Map<SaleDocumentDto>(entity);
+    //}
+}
